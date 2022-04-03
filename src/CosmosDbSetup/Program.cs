e496570a7c1220a262dc9a668ae4fb4182c5f058
Robin-Manuel Thiel <robin-manuel@thiel1.de>
2022-04-03 16:56:37 +0200

    Add Retry Policy

--- src/CosmosDbSetup/Program.cs
+++ src/CosmosDbSetup/Program.cs
@@ -1,14 +1,11 @@
 ﻿using CosmosDbSetup;
 using Microsoft.Azure.Cosmos;
+using Polly;
 
 string connectionString = Helpers.GetFromEnv("CONNECTION_STRING", "AccountEndpoint=https://cosmosdb:8081/;AccountKey=C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==");
 string databaseName = Helpers.GetFromEnv("DATABASE_NAME", "database");
 string containerName = Helpers.GetFromEnv("CONTAINER_NAME", "container");
 string containerPartitionKey = Helpers.GetFromEnv("CONTAINER_PARTITION_KEY", "/id");
-string delay = Helpers.GetFromEnv("DELAY", "0");
-
-Console.WriteLine($"Waiting {delay}ms...");
-Thread.Sleep(int.Parse(delay));
 
 CosmosClient client = new CosmosClient(connectionString, new CosmosClientOptions()
 {
@@ -23,8 +20,14 @@ CosmosClient client = new CosmosClient(connectionString, new CosmosClientOptions
     ConnectionMode = ConnectionMode.Gateway
 });
 
-await client.CreateDatabaseIfNotExistsAsync(databaseName);
-var database = client.GetDatabase(databaseName);
-await database.CreateContainerIfNotExistsAsync(containerName, containerPartitionKey);
+await Policy
+    .Handle<Exception>()
+    .WaitAndRetryAsync(5, retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)))
+    .ExecuteAsync(async () =>
+    {
+        await client.CreateDatabaseIfNotExistsAsync(databaseName);
+        var database = client.GetDatabase(databaseName);
+        await database.CreateContainerIfNotExistsAsync(containerName, containerPartitionKey);
+    });
 
 Console.WriteLine($"Done.");
